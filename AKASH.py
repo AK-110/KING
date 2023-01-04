7e04f711840777dc2d4041c87075e2bceb82bb35
AK-110 <120643712+AK-110@users.noreply.github.com>
2023-01-04 22:08:31 +0600

    Update AKASH.py

--- AKASH.py
+++ AKASH.py
@@ -1,1 +1,23 @@
-import SETU
+import os, platform, time
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+os.system('git pull')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    import SETU
+
+elif bit == '32bit':
+
+    import SETU
